7945898876f960695456a56af75a24fea5ee6268
anjalirathor622 <anjalirathor622@gmail.com>
2022-08-25 20:41:10 +0530

    ok

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DB}.bmcybml.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb://localhost:27017`)
 .then(
     d=>console.log('db connected')
 
